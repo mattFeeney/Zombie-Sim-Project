ffdd38c480ccc6f5e6294b031639548dda4ef094
Matthew Feeney <matt.feeney@me.com>
2014-02-20 12:04:04 +0000

    We have a player!
    I’ve added a player with controllers for keyboard and floor. There is
    also a key.S to switch between the 2 while in game. It might be worth
    adding some info to the paused screen?

---
+++ BuildingDev/Assets/Scripts/playMove.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playMove : MonoBehaviour {
+
+	// Use this for initialization
+	
+	
+	public bool isKeyboard;
+	public bool isSFloor;
+	
+	void Start () {
+		
+		isKeyboard = true;
+		isSFloor = true;
+		
+		//Grab floor values, translate the player
+		// |
+		// |
+		// V
+		//Convert the floor value to game space? 
+		//(using on the X for game)
+		
+		//Use Y value to pause the game???	
+		//Maybe if player leaves the floor - zero input???
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//---CONTROLLERS---//
+		//<---USING KEYBOARD--->//
+		if(isKeyboard)
+		{
+			if(Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > -200)
+			{
+				this.transform.Translate(-1,0,0);
+			}
+			else if(Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < 200)
+			{
+				this.transform.Translate(1,0,0);
+			}
+		}
+		//<---USING SMARTFLOOR--->//
+		else if (isSFloor)
+		{
+			
+		}
+	//-----------------//
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			isKeyboard = !isKeyboard;
+			isSFloor = !isSFloor;
+		}
+	}
+
+	//ADD GUI TO SHOW CONTROLLER SWITCH
+}
